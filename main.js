0b4ae4f7a1940a622b9f9ac5f959b648c160f81a
Ali <alkhdyyr@gmail.com>
2023-07-31 21:48:15 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+const passwordBox = document.getElementById("password");
+const lenght = 12;
+const upperCass = "ABCDEFGHIGKLMNOPQRSTUVWXYZ"  ; 
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "@#$%|><_-()[{]}/=";
+
+const allChar = upperCass + lowerCase + number + symbol ;
+
+function createPassword(){
+    let password = "";
+    password+= upperCass[Math.floor(Math.random() * upperCass.length)];
+    password+= lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password+= number[Math.floor(Math.random() * number.length)];
+    password+= symbol[Math.floor(Math.random() * symbol.length)];
+
+    while(lenght > password.length){
+        password+= allChar[Math.floor(Math.random() * allChar.length)];
+           
+    } 
+    passwordBox.value = password;
+}
+function copyPassword(){
+    passwordBox.select();
+    document.execCommand("copy");
+}
